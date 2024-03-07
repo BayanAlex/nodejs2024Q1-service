9ded0562613d5df8871db24666cea10f2ed303de
Oleksandr Shyhyda <BayanAlex@gmail.com>
2024-03-07 13:19:34 +0200

    fix: removed unused code

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,9 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { v4 as genUuid } from 'uuid';
 
 @Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Hello World!';
-  }
-}
+export class AppService {}
