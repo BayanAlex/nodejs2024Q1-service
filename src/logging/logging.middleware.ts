28e3f7989aa38c13863b2cd6615249a67453f271
Oleksandr Shyhyda <BayanAlex@gmail.com>
2024-03-31 21:48:29 +0300

    feat: added logging of unauthorized requests

--- src/logging/logging.middleware.ts
+++ src/logging/logging.middleware.ts
@@ -2,6 +2,7 @@ import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 import { LoggingService } from 'src/logging/logging.service';
 import { getRequestLogParams } from './logging.interceptor';
+import { StatusCodes } from 'http-status-codes';
 
 @Injectable()
 export class LoggingMiddleware implements NestMiddleware {
@@ -11,7 +12,7 @@ export class LoggingMiddleware implements NestMiddleware {
     response.on('close', () => {
       const logParams = getRequestLogParams(request);
       const { statusCode } = response;
-      if (!request.route) {
+      if (!request.route || response.statusCode === StatusCodes.UNAUTHORIZED) {
         this.loggingService.error(...logParams, `Status code: ${statusCode}`);
       }
     });
